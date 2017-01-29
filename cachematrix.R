2ed2b6d731022f75027cbd89d2ef5bbd3f12f57f
Mouzri <mouzri.hasou@gmail.com>
2017-01-29 16:54:05 +0000

    inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix<-function(x=matrix())
+{
+        INV<-NULL
+        getmatrix<-function(){
+                x
+        }
+        setinverse<-function(y){
+                INV<<-y
+        }
+        getinverse<-function()INV
+        list(getM=getmatrix,setI=setinverse, getI=getinverse)
+}       
+cacheSolve<-function(x){
+        Invr<-x$getI()
+        if(!is.null(Invr)){
+                print("getting the inverse")
+                return(Invr)
+        }
+        Mat<-x$getM()
+        if(det(Mat)==0)
+        {
+                print("The matrix is not Invertible")
+                return()
+        }
+        else
+        {
+                inverse<-solve(Mat)
+                x$setI(inverse)
+                inverse        
+        }
+        
 }
